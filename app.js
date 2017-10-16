8672772641f9dbcd306c7979e06b11d990432f6c
Tomer <tomerl101@gmail.com>
2017-10-16 18:33:00 +0300

    v 1.1
    added a search option in the home page

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const request = require('request');
 const app = express ();
+app.set('view engine', 'ejs');
 
-app.get('/result', (req, res)=>{
-    request("https://omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb", (error, respone, body)=>{
+app.get('/', (req, res)=>{
+    res.render('home');
+});
+
+app.get('/show_movies', (req, res)=>{
+    //getting the query from the home form
+    let search= req.query.search
+    request("https://omdbapi.com/?s=" +search + "&apikey=thewdb", (error, respone, body)=>{
         if(!error && respone.statusCode == 200){
+            //parse the JSON data into an object
             let movieData = JSON.parse(body);
-            res.send(movieData);
+            res.render('show', {movieData: movieData});
         }
     });
 });
+
 
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
