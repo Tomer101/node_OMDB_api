8f4fa7a60e25712a219a1b01e31ab89ffe55c735
Tomer <tomerl101@gmail.com>
2017-10-31 17:28:15 +0200

    v1.4
    -added single movie info
    -new home page styling

--- app.js
+++ app.js
@@ -21,6 +21,20 @@ app.get('/show_movies', (req, res)=>{
     });
 });
 
+app.get('/show/:movie', (req, res)=>{
+    //getting the movie id from the route params
+    let movieId= req.params.movie
+    request(`https://omdbapi.com/?i=${movieId}&apikey=thewdb`, (error, respone, body)=>{
+        if(!error && respone.statusCode == 200){
+            //parse the JSON data into an object
+            let movieData = JSON.parse(body);
+            console.log(movieData);
+            res.render('movie_info',{movieData: movieData});
+        }
+    });
+});
+
+
 
 app.listen(3000, ()=>{
     console.log('server is running on port 3000');
